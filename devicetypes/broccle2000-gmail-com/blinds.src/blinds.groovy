abcad4b9020cba10b518afa223c2c54104717d7d
John <broccle_2000@yahoo.com>
2016-11-26 02:56:34 -0800

    http://10.0.1.43:7474/code/Blinds%20Up

---
+++ devicetypes/broccle2000-gmail-com/blinds.src/blinds.groovy
@@ -0,0 +1,51 @@
+/**
+ *  Blinds
+ *
+ *  Copyright 2016 John
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "Blinds", namespace: "broccle2000@gmail.com", author: "John") {
+		capability "Momentary"
+
+		attribute "Blinds up", "string"
+
+		command "Up"
+	}
+
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'Blinds up' attribute
+
+}
+
+// handle commands
+def push() {
+	log.debug "Executing 'push'"
+	// TODO: handle 'push' command
+}
+
+def Up() {
+	log.debug "Executing 'Up'"
+	// TODO: handle 'Up' command
+}
